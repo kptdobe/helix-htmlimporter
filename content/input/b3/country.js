b75c5e3f378fb17959e716d235d5118d3eadb34c
Alexandre Capt <acapt@adobe.com>
2019-11-22 09:12:27 +0100

    Restructure + more examples

---
+++ content/input/b3/country.js
@@ -0,0 +1,12 @@
+(function () {
+	var country = {'code':'ch','id':21,'defaultLangauge':'fr-ch'};
+	
+	if (!window.evidon) window.evidon = {};
+	
+	if (window.evidon.notice) {
+		window.evidon.notice.setLocation(country);
+	}
+	else {
+		window.evidon.location = country;
+	}
+})();
--- index.js
+++ index.js
@@ -8,31 +8,27 @@ const fs = require('fs').promises;
 
 const { JSDOM } = jsdom;
 
+const OUTPUT_PATH = 'content/output'
+const OUTPUT_AUTHORS = `${OUTPUT_PATH}/authors`;
+const OUTPUT_POSTS = `${OUTPUT_PATH}/posts`;
+const OUTPUT_PRODUCTS = `${OUTPUT_PATH}/products`;
+const OUTPUT_TOPICS = `${OUTPUT_PATH}/topics`;
 
-async function main() {
-    const html = await fs.readFile('content/input/b1.html', 'utf8');
-    const dom = new JSDOM(html);
-    const { document } = dom.window;
-    const $ = jquery(document.defaultView);
-    
-    // remove nav
-    $('#article-nav-wrap').remove();
-    // remove 'products in article'
-    $('.article-body-products').remove();
-    
-    const $main = $('.main-content');
-    const $authorWrap = $('.article-author-wrap');
-    $main.append($authorWrap);
-    $('<hr>').insertBefore($authorWrap);
-    $('<hr>').insertAfter($authorWrap);
+async function getBlogPages() {
+    // TODO: fetch directly from website
+    return [{
+        html: await fs.readFile('content/input/b1.html', 'utf8'),
+        name: 'b1',
+    }, {
+        html: await fs.readFile('content/input/b2.html', 'utf8'),
+        name: 'b2',
+    }, {
+        html: await fs.readFile('content/input/b3.html', 'utf8'),
+        name: 'b3',
+    }]
+}
 
-    const $footer = $('.article-footer');
-    $main.append($footer);
-    $('<hr>').insertAfter($footer);
-
-
-    const content = $main.html();
-
+async function createPost(content) {
     unified()
         .use(parse, { emitParseErrors: true, duplicateAttribute: false })
         .use(rehype2remark)
@@ -41,10 +37,41 @@ async function main() {
             if (err) {
                 console.error(err);
             } else {
-                console.log(String(file))
-                await fs.writeFile('content/output/b1.md', file.contents);
+                // TODO deal with images
+                await fs.writeFile(`${OUTPUT_POSTS}/${name}.md`, file.contents);
+                console.log(`${OUTPUT_POSTS}/${name}.md file created`);
             }
         });
+}
+
+async function main() {
+    (await getBlogPages()).forEach((page) => {
+        const { name, html } = page;
+
+        const dom = new JSDOM(html);
+        const { document } = dom.window;
+        const $ = jquery(document.defaultView);
+        
+        // remove nav
+        $('#article-nav-wrap').remove();
+        // remove 'products in article'
+        $('.article-body-products').remove();
+        
+        const $main = $('.main-content');
+        const $authorWrap = $('.article-author-wrap');
+        $main.append($authorWrap);
+        $('<hr>').insertBefore($authorWrap);
+        $('<hr>').insertAfter($authorWrap);
+
+        const $footer = $('.article-footer');
+        $main.append($footer);
+        $('<hr>').insertAfter($footer);
+
+
+        const content = $main.html();
+
+        createPost();
+    });
 }
 
 main();
