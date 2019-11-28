bc166bc14ef9c2ca480f996fd2cbec1af0a32944
Alexandre Capt <acapt@adobe.com>
2019-11-28 15:05:14 +0100

    Split reusable code

---
+++ src/importer.js
@@ -0,0 +1,111 @@
+const unified = require("unified");
+const parse = require('rehype-parse')
+const rehype2remark = require('rehype-remark')
+const stringify = require('remark-stringify')
+const fs = require('fs-extra');
+const os = require('os');
+const path = require('path');
+const scrape = require('website-scraper');
+const byTypeFilenameGenerator = require('website-scraper/lib/filename-generator/by-type');
+const SaveToExistingDirectoryPlugin = require('website-scraper-existing-directory');
+
+const { asyncForEach } = require('./utils');
+
+async function getPages(urls) {
+    const options = {
+        urls: urls,
+        directory: await fs.mkdtemp(path.join(os.tmpdir(), 'htmlimporter-get-pages-')),
+        recursive: false,
+        urlFilter: function(url) {
+            return url.indexOf('adobe') !== -1;
+        },
+        sources: [
+            // only keep the imgs
+            { selector: 'img', attr: 'src' },
+            {selector: '[style]', attr: 'style'},
+		    {selector: 'style'},
+        ],
+        subdirectories: [
+            { directory: 'img', extensions: ['.git', '.jpg', '.jpeg', '.png', '.svg'] },
+        ],
+        plugins: [
+            new (class {
+                apply(registerAction) {
+                    let occupiedFilenames, defaultFilename, rootDirectory;
+
+                    registerAction('beforeStart', ({options}) => {
+                        occupiedFilenames = [];
+                        defaultFilename = options.defaultFilename;
+                        rootDirectory = options.directory;
+                    });
+                    registerAction('generateFilename', async ({ resource }) => {
+                        if ('index.html' === resource.filename) {
+                            // replace index.html by last segment in path
+                            // and remove last / if it is the last character
+                            const u = resource.url.replace('/index.html','').replace(/\/$/g,'');
+                            const name = u.substring(u.lastIndexOf('/') + 1, u.length);
+                            resource.localPath = `${rootDirectory}/${name}.html`;
+                            return { filename: `${name}.html` };
+                        }
+
+                        let directory = 'img';
+                        if ( resource.parent ) {
+                            const u = resource.parent.url.replace('/index.html','').replace(/\/$/g,'');
+                            directory = u.substring(u.lastIndexOf('/') + 1, u.length);
+                        }
+
+                        // else default behavior
+                        const filename = byTypeFilenameGenerator(resource, { 
+                            subdirectories: [ { directory, extensions: ['.git', '.jpg', '.jpeg', '.png', '.svg'] } ],
+                            defaultFilename }, occupiedFilenames);
+                        occupiedFilenames.push(filename);
+                        resource.localPath = `${rootDirectory}/${filename}`;
+                        return { filename };
+                    });
+                }
+            })(),
+            new SaveToExistingDirectoryPlugin()
+        ]
+    };
+    
+    console.log(`Starting to scrape urls ${urls.join(',')}`);
+    const result = await scrape(options);
+    console.log(`Done with scraping. Downloaded ${result.length} page(s).`);
+
+    return result;
+};
+
+async function createMarkdownFile(directory, name, content, links = []) {
+    console.log(`Creating a new MD file: ${directory}/${name}.md`);
+    await unified()
+        .use(parse, { emitParseErrors: true, duplicateAttribute: false })
+        .use(rehype2remark)
+        .use(stringify)
+        .process(content)
+        .then(async (file) => {
+            const p = `${directory}/${name}.md`;
+            await fs.mkdirs(`${directory}`);
+            await fs.writeFile(p, file.contents);
+            console.log(`MD file created: ${p}`);
+
+            if (links && links.length > 0) {
+                const folder = `${directory}/${name}`;
+                await fs.mkdirs(folder);
+                // copy resources (imgs...) folder
+                await asyncForEach(links, async (l) => {
+                    const rName = path.parse(l.url).base;
+                    // try to be smart, only copy images "referenced" in the content
+                    if (l.saved && file.contents.indexOf(rName) !== -1) {
+                        await fs.copy(`${l.localPath}`, `${folder}/${rName}`);
+                    }
+                });
+            }
+        });
+}
+
+async function createMarkdownFileFromResource(directory, resource, content) {
+    const name = path.parse(resource.filename).name
+    await createMarkdownFile(directory, name, content, resource.children);
+}
+
+module.exports = { getPages, createMarkdownFile, createMarkdownFileFromResource };
---
+++ src/index.js
@@ -0,0 +1,136 @@
+const jquery = require('jquery');
+const jsdom = require('jsdom');
+const fs = require('fs-extra');
+const path = require('path');
+
+const { asyncForEach } = require('./utils');
+const { getPages, createMarkdownFileFromResource } = require('./importer');
+
+const { JSDOM } = jsdom;
+
+const OUTPUT_PATH = 'content/output'
+
+const TYPE_AUTHOR = 'authors';
+const TYPE_POST = 'posts';
+const TYPE_PRODUCT = 'products';
+const TYPE_TOPIC = 'topics';
+
+const URLS = [
+    'https://theblog.adobe.com/conversion-success-events/',
+    'https://theblog.adobe.com/new-data-governance-capabilities-in-adobe-experience-platform-help-brands-manage-data-better',
+    'https://theblog.adobe.com/four-paintings-that-show-what-you-can-do-in-adobe-fresco',
+    'https://theblog.adobe.com/how-to-build-a-best-of-breed-product-offering',
+    'https://theblog.adobe.com/weve-achieved-global-gender-pay-parity-a-milestone-worth-celebrating/',
+    'https://theblog.adobe.com/leadership-lessons-from-the-u-s-womens-national-soccer-team/',
+    'https://theblog.adobe.com/adobe-breaks-ground-on-north-tower-in-san-jose/',
+    'https://theblog.adobe.com/recovering-reservations-from-visitors-who-abandon-the-hotel-reservation-process/',
+    'https://theblog.adobe.com/helping-travelers-find-personal-dream-vacations/',
+    'https://theblog.adobe.com/creating-adobe-experience-platform-pipeline-with-kafka/',
+    'https://theblog.adobe.com/how-marketo-engage-helped-icf-align-sales-and-marketing/'
+];
+
+async function handleAuthor($) {
+    const postedBy = $('.author-link').text();
+    const authorLink = $('.author-link')[0].href;
+    const postedOn = $('.post-date').text().toLowerCase();
+    
+    const $p = $('<p>');
+    $p.append(`by ${postedBy}<br>${postedOn}`);
+
+    const authorFilename  = path.parse(authorLink).name;
+    const fullPath = `${OUTPUT_PATH}/${TYPE_AUTHOR}/${authorFilename}.md`;
+    if (!await fs.exists(fullPath)) {
+        console.log(`File ${fullPath} does not exist. Retrieving it now.`);
+        await asyncForEach(await getPages([authorLink]), async (resource) => {
+            const text = await fs.readFile(`${resource.localPath}`, 'utf8');
+            const dom = new JSDOM(text);
+            const { document } = dom.window;
+            const $ = jquery(document.defaultView);
+            
+            const $main = $('.author-header');
+
+            // convert author-img from div to img for auto-processing
+            const $div = $('.author-header .author-img');
+            const urlstr = $div.css('background-image');
+            const url = /\(([^)]+)\)/.exec(urlstr)[1];
+            $main.prepend(`<img src="${url}">`);
+            $div.remove();
+
+            const content = $main.html();
+            await createMarkdownFileFromResource(`${OUTPUT_PATH}/${TYPE_AUTHOR}`, resource, content);
+        });
+    } else {
+        console.log(`File ${fullPath} exists, no need to compute it again.`);
+    }
+
+    return $p;
+}
+
+async function handleTopics($) {
+    let topics = '';
+    $('.article-footer-topics-wrap .text').each((i, t) => {
+        topics += `${t.innerHTML}, `;
+    });
+
+    topics = topics.slice(0, -2); 
+    
+    const $p = $('<p>');
+    $p.append(`Topics: ${topics}`);
+
+    await asyncForEach(
+        topics.split(',')
+            .filter(t => t && t.length > 0)
+            .map(t => t.trim()),
+        async (t) => {
+            const fullPath = `${OUTPUT_PATH}/${TYPE_TOPIC}/${t.replace(/\s/g,'-').toLowerCase()}.md`
+            if (!await fs.exists(fullPath)) {
+                console.log(`Found a new topic: ${t}`);
+                await createMarkdownFileFromResource(`${OUTPUT_PATH}/${TYPE_TOPIC}`, {
+                    filename: `${t.replace(/\s/g,'-').toLowerCase()}.md`,
+                }, `<h1>${t}</h1>`);
+            } else {
+                console.log(`Topic already exists: ${t}`);
+            }
+        });
+
+    return $p;
+}
+
+async function main() {
+    await fs.remove(OUTPUT_PATH);
+
+    await asyncForEach(await getPages(URLS), async (resource) => {
+        // encoding issue, do not use resource.text
+        const text = await fs.readFile(`${resource.localPath}`, 'utf8');
+        const dom = new JSDOM(text);
+        const { document } = dom.window;
+        const $ = jquery(document.defaultView);
+        
+        const $main = $('.main-content');
+
+        const $newAuthorWrap = await handleAuthor($);
+        $main.append($newAuthorWrap);
+        $('<hr>').insertBefore($newAuthorWrap);
+
+        const $newTopicWrap = await handleTopics($);
+        $main.append($newTopicWrap);
+        $('<hr>').insertBefore($newTopicWrap);
+
+        // remove author / products section
+        $('.article-author-wrap').remove();
+        // remove footer
+        $('.article-footer').remove();
+        // remove nav
+        $('#article-nav-wrap').remove();
+        // remove 'products in article'
+        $('.article-body-products').remove();
+
+        const content = $main.html();
+
+        await createMarkdownFileFromResource(`${OUTPUT_PATH}/${TYPE_POST}`, resource, content);
+    });
+
+    console.log('Process done!');
+}
+
+main();
